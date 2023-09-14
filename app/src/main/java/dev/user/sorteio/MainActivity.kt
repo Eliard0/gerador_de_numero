261069c1301e1cda3b08bd093d8597ead9247155
eliard0 <eliardosilva81@gmail.com>
2023-09-14 15:51:01 -0300

    front end tela de login e principal ok

---
+++ app/src/main/java/dev/user/sorteio/MainActivity.kt
@@ -0,0 +1,29 @@
+package dev.user.sorteio
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import dev.user.sorteio.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        supportActionBar?.hide()
+
+        binding.btInicia.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.bt_inicia) {
+            startActivity(Intent(this, HomeActivity::class.java))
+        }
+    }
+}
